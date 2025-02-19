121d41b9f922c08d6924abc5d93e060a84381ee7
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2025-02-18 21:21:57 -0300

    pages

---
+++ src/screens/passenger/passenger.style.js
@@ -0,0 +1,33 @@
+export const styles = {
+    container: {
+        flex: 1
+    },
+    map: {
+        flex: 1,
+        width: "100%"
+    },
+    marker: {
+        width: 60,
+        height: 60
+    },
+    footer: {
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    footerFields: {
+        margin: 10
+    },
+    footerText: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 10
+    },
+    input: {
+        backgroundColor: "#fff",
+        borderWidth: 1,
+        borderColor: "#cccccc",
+        padding: 10,
+        width: 300
+    }
+};
