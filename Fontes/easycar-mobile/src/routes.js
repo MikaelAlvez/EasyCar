fcec03179d11d351c8bcba5911679fa2441c485e
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2025-02-27 08:39:41 -0300

    LoadScreen

--- Fontes/easycar-mobile/src/routes.js
+++ Fontes/easycar-mobile/src/routes.js
@@ -2,7 +2,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from "./screens/home/home.jsx"
 import Passenger from "./screens/passenger/passenger.jsx";
-import Login from "./screens/login/login.jsx";
 import Ride from "./screens/ride/ride.jsx"
 import RideDetail from "./screens/rideDetail/rideDetail.jsx";
 
@@ -11,8 +10,8 @@ const Stack = createNativeStackNavigator();
 function Routes(){
     return <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name="home" component={Home} options={{headerShadowVisible: false, headerTitle: "", headerTransparent: true}}/>
-            <Stack.Screen name="login" component={Login} options={{headerShown: false}}/>
+            <Stack.Screen name="home" component={Home} options={{headerShown: false}}/>
+            {/*<Stack.Screen name="login" component={Login} options={{headerShown: false}}/>*/}
             <Stack.Screen name="passenger" component={Passenger} options={{headerShadowVisible: false, headerTitle: "", headerTransparent: true}}/>
             <Stack.Screen name="ride" component={Ride} options={{headerTitle: "Viagens disponíveis", headerTitleAlign: "center"}}/>
             <Stack.Screen name="rideDetail" component={RideDetail} options={{headerTitle: "", headerTitleAlign: "center", headerTransparent: true}}/>
--- Fontes/easycar-mobile/src/screens/passenger/passenger.jsx
+++ Fontes/easycar-mobile/src/screens/passenger/passenger.jsx
@@ -2,13 +2,48 @@ import MyButton from "../../components/mybutton/mybutton";
 import { Text, TextInput, View } from "react-native";
 import MapView, {Marker, PROVIDER_DEFAULT} from "react-native-maps";
 import { styles } from "./passenger.style.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Passenger(props){
     const [myLocation, setMyLocation] = useState({
         latitude: 20,
         longitude: 20
     });
+
+    const [title, setTitle] = useState("");
+
+    async function LoadScreen(){
+        const response = await RequestRideFromUser();
+
+        if(!response.ride_id){
+            setTitle("Encontre a sua carona agora");
+        } else {
+
+        }
+    }
+
+    async function RequestRideFromUser(){
+        const response = {
+            /*ride_id: 1,
+            passenger_user_id: 1,
+            passenger_name: "Carlos Alberto de Nobrega",
+            passenger_phone: "(11)1111-1111",
+            pickup_address: "Rua A - Brahma",
+            pickup_date: "2025-02-19",
+            pickup_latitute: "-5.20559",
+            pickup_longitude: "-37.33444",
+            dropoff_address: "Shopping popular",
+            status: "Pendente",
+            driver_user_id: null,
+            driver_name: null,
+            driver_phone: null*/
+        }
+        return response;
+    }
+
+    useEffect(() => {
+        LoadScreen();
+    }, [])
 
     return <View style={styles.container}>
 
@@ -28,11 +63,11 @@ function Passenger(props){
                 }}
                 title="Passageiro"
                 description="Mossoró/RN"
-                />
+                />  
         </MapView>
         <View style={styles.footer}>
                 <View style={styles.footerText}>
-                    <Text>Encontre a sua carona</Text>
+                    <Text>{title}</Text>
                 </View>
                 <View style={styles.footerFields}>
                     <Text>Origem</Text>
@@ -44,7 +79,7 @@ function Passenger(props){
                 </View>
         </View>
 
-        <MyButton text="CONFIRMAR" theme=""></MyButton>
+        <MyButton text="CONFIRMAR" theme="default"></MyButton>
 
     </View>
 
